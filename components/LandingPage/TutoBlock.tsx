234aa0fe2ae90a69e93a4bb0a851085e060e6f1a
d-s-i <briacgoujon95@gmail.com>
2021-11-30 19:14:37 +0100

    resolving key error

--- components/LandingPage/TutoBlock.tsx
+++ components/LandingPage/TutoBlock.tsx
@@ -14,46 +14,46 @@ const steps = [
     {
         title: "Join the Community",
         bullets: [
-            <span>Join the <a href="https://t.me/yeroglyphs" style={{fontWeight: "bold", color: "#ccad00"}} target="_blank" rel="noopener noreferrer">Telegram</a> and engage with your fellow historians</span>,
-            <span>Follow us on <a href="https://twitter.com/yeroglyphs" style={{fontWeight: "bold", color: "#ccad00"}} target="_blank" rel="noopener noreferrer" >Twitter</a>, many of the passwords will be hidden there</span>,
-            <span>Check the history of the messages, rewteet, like and comment our tweets</span>,
-            <span>Be curious, our founder is quite sadistic when it comes to hiding clues.</span>,
-            <span>Try your luck to find a hidden password and use it to mint a Genesis Yero</span>
+            <span key="bullet-1" >Join the <a href="https://t.me/yeroglyphs" style={{fontWeight: "bold", color: "#ccad00"}} target="_blank" rel="noopener noreferrer">Telegram</a> and engage with your fellow historians</span>,
+            <span key="bullet-2" >Follow us on <a href="https://twitter.com/yeroglyphs" style={{fontWeight: "bold", color: "#ccad00"}} target="_blank" rel="noopener noreferrer" >Twitter</a>, many of the passwords will be hidden there</span>,
+            <span key="bullet-3" >Check the history of the messages, rewteet, like and comment our tweets</span>,
+            <span key="bullet-4" >Be curious, our founder is quite sadistic when it comes to hiding clues.</span>,
+            <span key="bullet-5" >Try your luck to find a hidden password and use it to mint a Genesis Yero</span>
         ]
     }, {
         title: "Mint Your Own Yero NFT",
         bullets: [
-            <span>Click on any button on this page to go on the minting page</span>,
-            <span>Connect your wallet</span>,
-            <span>Make sure there are still Yero available</span>,
-            <span>Click “Mint”</span>,
-            <span>Congrats! You you created unique art and started your journey in generative art</span>
+            <span key="bullet-6" >Click on any button on this page to go on the minting page</span>,
+            <span key="bullet-7" >Connect your wallet</span>,
+            <span key="bullet-8" >Make sure there are still Yero available</span>,
+            <span key="bullet-9" >Click “Mint”</span>,
+            <span key="bullet-10" >Congrats! You you created unique art and started your journey in generative art</span>
         ]   
     }, {
         title: "Observe The Art Your Generated",
         bullets: [
-            <span>Nft plateform are not ready yet on Boba, so we created ours !</span>,
-            <span>Go to NOT_AVAILABLE_YET</span>,
-            <span>Connect your Metamask</span>,
-            <span>Admire the art you created!</span>
+            <span key="bullet-11" >Nft plateform are not ready yet on Boba, so we created ours !</span>,
+            <span key="bullet-12" >Go to NOT_AVAILABLE_YET</span>,
+            <span key="bullet-13" >Connect your Metamask</span>,
+            <span key="bullet-14" >Admire the art you created!</span>
         ]
     }, {
         title: "Generate New Art On Your NFT",
         bullets: [
-            <span>Side note : You always keep previously generated art stored in your NFT contract !</span>,
-            <span>Go on NOT_AVAILABLE_YET</span>,
-            <span>Vizualize the current state of your NFT (it changes on every block)</span>,
-            <span>If you want to save this state, click the button !</span>,
-            <span>As simple as this!</span>
+            <span key="bullet-15" >Side note : You always keep previously generated art stored in your NFT contract !</span>,
+            <span key="bullet-16" >Go on NOT_AVAILABLE_YET</span>,
+            <span key="bullet-17" >Vizualize the current state of your NFT (it changes on every block)</span>,
+            <span key="bullet-18" >If you want to save this state, click the button !</span>,
+            <span key="bullet-19" >As simple as this!</span>
         ]
     }, {
         title: "Save This Art As Default",
         bullets: [
-            <span>If you want to put your newly generated art as default on your favorite plateform, you can!</span>,
-            <span>Go on NOT_AVAILABLE_YET</span>,
-            <span>Check all the artwork your created</span>,
-            <span>Click 'Save' on the artwork you like</span>,
-            <span>Congrats! You just changed the image of your NFT</span>
+            <span key="bullet-20" >If you want to put your newly generated art as default on your favorite plateform, you can!</span>,
+            <span key="bullet-21" >Go on NOT_AVAILABLE_YET</span>,
+            <span key="bullet-22" >Check all the artwork your created</span>,
+            <span key="bullet-23" >{`Click 'Save' on the artwork you like`}</span>,
+            <span key="bullet-24" >Congrats! You just changed the image of your NFT</span>
         ]
     }, 
     {
