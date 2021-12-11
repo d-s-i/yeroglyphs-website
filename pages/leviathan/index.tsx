50a3ca8595a220f19f5b424e445bf3aeb62343dc
d-s-i <briacgoujon95@gmail.com>
2021-12-11 10:34:48 +0100

    removing quotes

--- pages/leviathan/index.tsx
+++ pages/leviathan/index.tsx
@@ -14,12 +14,12 @@ export default function Leviathan() {
           </Typography>
         </Container> */}
         <Typography component="p" variant="h6" color="primary" sx={{ marginTop: "30%" }}>{`In the Old Testament we read how Yahweh protected mortals from a terrible sea monster, Leviathan. It seems that Leviathan could once again come out of its lair and ravage the Egyptian shores. To avoid this catastrophe, Cleopatra gave you a stone engraved with the words of Yahweh himself about Leviathan: `}</Typography>
-        <Typography component="p" variant="h6" color="primary" align="center" sx={{ marginTop: "5%" }}>"His heart is as hot and hard as </Typography>
+        <Typography component="p" variant="h6" color="primary" align="center" sx={{ marginTop: "5%" }}>&ldquo;His heart is as hot and hard as </Typography>
         <Typography component="p" variant="h6" color="primary" align="center">a stone left in the desert sun.</Typography>
         <Typography component="p" variant="h6" color="primary" align="center">He howls all day long, and the sailors hear his bitter fury </Typography>
         <Typography component="p" variant="h6" color="primary" align="center">thrown into the sea at nightfall. </Typography>
         <Typography component="p" variant="h6" color="primary" align="center">And on land, no one can be born </Typography>
-        <Typography component="p" variant="h6" color="primary" align="center">to become its master."</Typography>
+        <Typography component="p" variant="h6" color="primary" align="center">to become its master.&rdquo;</Typography>
         <Typography component="p" variant="h6" color="primary" sx={{ marginTop: "5%" }}>In order to be fair and transparent for everyone, only answer on the telegram will be accepted! Answer here: t.me/yeroglyphs</Typography>
     </Container>
   );
